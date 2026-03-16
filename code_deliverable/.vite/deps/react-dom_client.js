4ab8f5b21c48b434a4defb7b1b00b7fa696a1804
Hauke Sandhaus <hgs52@cornell.edu>
2026-03-16 18:43:36 -0400

    update .gitignore and remove accidental .vite files

--- code_deliverable/.vite/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-MEI7MJPF.js";
-import "./chunk-6YOVXODW.js";
-import "./chunk-VX2H6PUQ.js";
-import "./chunk-G3PMV62Z.js";
-export default require_client();
